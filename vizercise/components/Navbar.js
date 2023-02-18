c4843200c3b8202236fd4e4729cef862bedbf33f
Ernest Umeh <umeh@kth.se>
2023-02-18 10:15:31 +0100

    feat: Add tailwind to app and structure homepage

---
+++ vizercise/components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+      <>
+        {/* <nav>
+            <Link href="/">
+                Home
+            </Link>
+        </nav> */}
+      </>
+    );
+  }
