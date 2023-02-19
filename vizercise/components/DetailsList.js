e750b22a68e42564e6d5eea3c0ccba3ea51a090d
Måns Nyman <m.nyman@hotmail.com>
2023-02-19 21:03:43 +0100

    add minimal styling details table

--- vizercise/components/DetailsList.js
+++ vizercise/components/DetailsList.js
@@ -8,26 +8,55 @@ export default function DetailsList(props) {
 
     function drawTable(exerciseId) {
         return (
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>Exercise name</th>
-                        <th>Equipment</th>
-                        <th>Difficulty</th>
-                        <th>Mechanic</th>
-                        <th>Force</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{GetExerciseById(exerciseId).name}</td>
-                        <td>{GetExerciseById(exerciseId).equipment}</td>
-                        <td>{GetExerciseById(exerciseId).difficulty}</td>
-                        <td>{GetExerciseById(exerciseId).mechanic}</td>
-                        <td>{GetExerciseById(exerciseId).force}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="flex flex-col">
+                <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden">
+                            <table class="min-w-full">
+                                <thead class="border-b">
+                                    <tr>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Exercise name
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Equipment
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Difficulty
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Mechanic
+                                        </th>
+                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                            Force
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            {GetExerciseById(exerciseId).name}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {GetExerciseById(exerciseId).equipment}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {GetExerciseById(exerciseId).difficulty}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {GetExerciseById(exerciseId).mechanic}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {GetExerciseById(exerciseId).force}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
         )
     }
 
