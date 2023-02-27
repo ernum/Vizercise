68ef26a419ebb6409bbbbb4e84d28709cb3d23e4
carterwsmith <csmithonthetrack@gmail.com>
2023-02-27 16:46:12 +0100

    fix: overflow issue on table view

--- vizercise/components/DetailsList.js
+++ vizercise/components/DetailsList.js
@@ -9,7 +9,7 @@ export default function DetailsList(props) {
 
   function drawTable(exerciseId) {
     return (
-      <div className='flex flex-col'>
+      <div className='overflow-x-auto flex flex-col'>
         <div className='overflow-x-auto sm:-mx-6 lg:-mx-8'>
           <div className='py-2 inline-block min-w-full sm:px-6 lg:px-8'>
             <div className='overflow-hidden'>
