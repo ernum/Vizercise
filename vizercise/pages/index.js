ed9508c91b561fce6ce40cdf96b8bb2fc3a750a8
carterwsmith <csmithonthetrack@gmail.com>
2023-03-02 11:34:59 +0100

    feat: update empty table view

--- vizercise/pages/index.js
+++ vizercise/pages/index.js
@@ -42,9 +42,14 @@ export default function Home() {
   function ExerciseVisualisation() {
     if (!exerciseSelected) {
       return (
-        <p className="absolute font-montserrat font-normal text-[32px] leading-10 left-[23.6%] top-[41.4%]">
+        <div class="h-full flex flex-col text-center items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" opacity="0.4" fill="none" stroke="#696969" viewBox="0 0 24 24" stroke-width="1.5" class="w-12 h-12">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+          </svg>
+          <p className="font-montserrat font-normal text-[20px] w-[50%]">
           Please select an exercise
-        </p>
+          </p>
+        </div>
       );
     } else {
       return (
