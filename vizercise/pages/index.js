7962fcf042000d74cb60c1efe48aca1ffcb4bbe3
carterwsmith <csmithonthetrack@gmail.com>
2023-03-02 11:50:45 +0100

    feat: refactor table rendering

--- vizercise/pages/index.js
+++ vizercise/pages/index.js
@@ -39,28 +39,6 @@ export default function Home() {
       exerciseId => exerciseId != id));
   }
 
-  function ExerciseVisualisation() {
-    if (!exerciseSelected) {
-      return (
-        <div className="h-full flex flex-col text-center items-center justify-center">
-          <svg xmlns="http://www.w3.org/2000/svg" opacity="0.4" fill="none" stroke="#696969" viewBox="0 0 24 24" strokeWidth="1.5" className="w-12 h-12">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
-          </svg>
-          <p className="font-montserrat font-normal text-[20px] w-[50%]">
-          Please select an exercise
-          </p>
-        </div>
-      );
-    } else {
-      return (
-        <DetailsList 
-          selectedExercises={selectedExercises}
-          onExerciseRemoval={removeExercise}
-        />
-      )
-    }
-  }
-
   return (
     <div>
       {/* Biggest Box */}
@@ -98,7 +76,10 @@ export default function Home() {
         bg-white border-[1px] border-solid border-[##CAC4C4] rounded-[30px] 
         shadown-black/25"
       >
-        {ExerciseVisualisation()}
+        <DetailsList 
+          selectedExercises={selectedExercises}
+          onExerciseRemoval={removeExercise}
+        />
       </div>
     </div>
   );
