386b9e216eca0612844165a0924baae3600d2eee
Ernest Umeh <umeh@kth.se>
2023-03-13 20:55:38 +0100

    feat: Add header and favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="icon" href="/icons/favicon-32x32.png" />
+        <title>Vizercise | Exercises Visualized with Interactive Body Maps</title>
+      </Head>
       <Component {...pageProps} />
     </div>
   );
