c56f34e9f3e38bcb206ed16dd5f040204bddf4be
modi-99 <78117231+modi-99@users.noreply.github.com>
2023-03-13 13:54:23 +0100

    commited
    video showing in the table view are now resizable and responsive, also added a slider bar to indacate current position in the video table

--- src/components/VideoView.js
+++ src/components/VideoView.js
@@ -9,26 +9,55 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function VideoView(props) {
+  const arr = props.selectedExercises;
   const arrLength = props.selectedExercises.length;
   var [index, setIndex] = useState(0);
 
   var videoURL = GetExerciseById(props.selectedExercises[index]).video;
   var exerciseName = GetExerciseById(props.selectedExercises[index]).name;
+  function slick() {
+    return (
+      <div className='flex justify-center items-center'>
+        {arr.map((NULL, i) => (
+          <div
+            className={`bg-gray-300 w-2 h-2 m-1 rounded-full
+    ${index === i ? "p-2" : ""}`}></div>
+        ))}
+      </div>
+    );
+  }
 
   function displayVideo() {
     return (
-      <div>
-        <h1 className='text-center p-2 text-3xl text-neutral-600 font-medium'>
-          {exerciseName}
-        </h1>
-        <iframe
-          className='m-auto rounded-xl'
-          src={videoURL}
-          width='500px'
-          height='282px'
-          allow='autoplay; encrypted-media'
-          allowFullScreen
-          title='video'></iframe>
+      <div className='flex justify-center gap-10 '>
+        <button onClick={handleDecrement}>
+          <FontAwesomeIcon
+            icon={faChevronLeft}
+            className='  text-gray-400 text-4xl hover:text-gray-800 transition duration-400'
+          />
+        </button>
+        <div className=' shadow-2xl top-0 left-1/4 w-1/2 p-3 rounded-lg'>
+          <h1 className='text-center text-2xl text-neutral-600 font-medium'>
+            {exerciseName}
+          </h1>
+          <div className='relative overflow-hidden pt-[56.60%]'>
+            <iframe
+              className=' absolute top-0 left-0 w-[100%] h-[100%] rounded-xl'
+              src={videoURL}
+              allow='autoplay; encrypted-media'
+              allowFullScreen
+              title='video'></iframe>
+          </div>
+
+          <div className=' items-end text-center'>{slick()}</div>
+        </div>
+
+        <button onClick={handleIncrement}>
+          <FontAwesomeIcon
+            icon={faChevronRight}
+            className=' text-gray-400 text-4xl hover:text-gray-800 transition duration-400'
+          />
+        </button>
       </div>
     );
   }
@@ -36,7 +65,7 @@ export default function VideoView(props) {
   function handleIncrement() {
     if (index !== arrLength - 1) {
       setIndex((previousState) => previousState + 1);
-    }
+    } else return setIndex(0);
   }
 
   function handleDecrement() {
@@ -46,32 +75,18 @@ export default function VideoView(props) {
   }
 
   return (
-    <div>
-      <div className='  m-5'>
-        <TableButton
-          view={"video"}
-          onSetExerciseOrVideo={props.onSetExerciseOrVideo}
-          bool={false}
-        />
-      </div>
-
-      <div className=' flex flex-row mx-40 justify-center '>
-        <button onClick={handleDecrement}>
-          <FontAwesomeIcon
-            icon={faChevronLeft}
-            className=' text-gray-400 text-4xl hover:text-gray-800 transition'
+    <>
+      <div className=' overflow-auto flex mx-6 my-4 '>
+        <div>
+          <TableButton
+            view={"video"}
+            onSetExerciseOrVideo={props.onSetExerciseOrVideo}
+            bool={false}
           />
-        </button>
-        <div className='shadow-2xl rounded-xl m-auto min-w-min p-3'>
-          <div>{displayVideo()}</div>
         </div>
-        <button onClick={handleIncrement}>
-          <FontAwesomeIcon
-            icon={faChevronRight}
-            className=' text-gray-400 text-4xl hover:text-gray-800 transition'
-          />
-        </button>
+
+        <div className=' flex-auto '>{displayVideo()}</div>
       </div>
-    </div>
+    </>
   );
 }
