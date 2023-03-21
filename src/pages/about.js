df4cb6e35bff7da38305484eb37fa11da18a8483
carterwsmith <csmithonthetrack@gmail.com>
2023-03-21 18:49:19 +0100

    doc: literary citations in about:

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,13 +7,17 @@ function About() {
                 <h1 className="text-2xl font-bold mb-2" style={{ marginTop: "20px", marginBottom: "15px", textAlign: "center" }}>About Us</h1>
                 <h2 className="text-center text-4x1 font-bold" style={{ marginBottom: "15px", marginTop: "10px", fontSize: "20px"}}>The Project</h2>
                     <div className="row" style={{ marginLeft: "20%", marginRight: "20%", textAlign: 'center', marginBottom: "10px" }}>
-                        Vizercise is an visualization tool that allows users to explore exercises based on activated muscle groups, exercise mechanics, difficulty, and other factors crucial to workout effectiveness [1]. 
-                        The motivation behind this project was to bring visual representation and interactivity to exercise data from <a className="text-blue-600" href="https://musclewiki.com" target="_blank" rel="noreferrer">MuscleWiki [2]</a> by mapping it to the human body and giving a visual representation of workout intensity [3].
+                        Vizercise is an visualization tool that allows users to explore exercises based on activated muscle groups, exercise mechanics, difficulty, and other factors crucial to workout effectiveness [1] using &quot;overview first, zoom and filter, then details-on-demand&quot; [2] interaction. 
+                        The motivation behind this project was to bring visual representation and interactivity to exercise data from <a className="text-blue-600" href="https://musclewiki.com" target="_blank" rel="noreferrer">MuscleWiki [3]</a> by giving it a visual representation of workout intensity [4].
+                        Our choice of the human body map was done based on the principle of visualization &quot;that to describe the visible world in images we need a developed system of schemata&quot; [5].
+                        ‎
                     </div>
                     <div className="row" style={{ marginLeft: "10%", marginRight: "10%", textAlign: 'center', marginBottom: "30px", fontSize: "10px" }}>
                         Schoenfeld, Brad J. The Mechanisms of Muscle Hypertrophy and Their Application to Resistance Training [1] <br />
-                        [2] MuscleWiki, https://musclewiki.com <br />
-                        Barbalho, Matheus, et al. &quot;Evidence for an upper threshold for resistance training volume in trained women.&quot; Med Sci Sports Exerc (2019) [3]
+                        [2] Shneiderman, Ben. &quot;The eyes have it: A task by data type taxonomy for information visualizations.&quot; (1996) <br />
+                        [3] MuscleWiki, https://musclewiki.com <br />
+                        Barbalho, Matheus, et al. &quot;Evidence for an upper threshold for resistance training volume in trained women.&quot; Med Sci Sports Exerc (2019) [4] <br />
+                        [5] Gombrich, E. H., Art and Illusion; a Study in the Psychology of Pictorial Representation. [Princeton, N.J.] :Princeton University Press, 1969.
                     </div>
 
                 <h2 className="text-center text-4x1 font-bold" style={{ marginBottom: "15px", marginTop: "10px", fontSize: "20px"}}>Our Team</h2>
