e54f3dbabd4c433f50a796ebe8fa705bdc7e637d
Hymalide <marine50799@gmail.com>
2023-03-21 20:05:39 +0100

    FINALY (pls tell me it is the last)

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,7 +20,7 @@ function About() {
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
                                     <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Carter Smith</h2>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Collection and Processing, Frontend Development, UI/UX Design</p>
-
+                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">I learned to work with React and improved my D3.js and JavaScript skills</p>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">carters@kth.se</p>
                                 <a href="https://www.linkedin.com/in/carterwsmith" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
@@ -32,7 +32,7 @@ function About() {
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
                                 <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Ernest Umeh</h2>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design</p>
-
+                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">I learned to work with D3.js and design websites with Figma, I also improved my JavaScript skills</p>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">umeh@kth.se</p>
                                 <a href="https://www.linkedin.com/in/ernest-umeh" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
@@ -44,7 +44,7 @@ function About() {
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
                                 <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Mohammed Qasim</h2>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design, User test</p>
-
+                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">I learned to work with React, D3.js and tailwind, but also improved my github usage skills</p>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">msyqasim@kth.se</p>
                                 <a href="https://www.linkedin.com/in/modi-qasim/" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
@@ -58,7 +58,7 @@ function About() {
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
                                 <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">M{"\u00E5"}ns Nyman</h2>
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design</p>
-
+                                    <p className="text-center p-2 text-neutral-600 py-2 font-normal">I really wanted to improve my interaction programming skills, and I believe I did</p>
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">mansnym@kth.se</p>
 
                                 </div>
@@ -71,7 +71,7 @@ function About() {
 
                                 <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Marine Czaplinski</h2>
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">Frontend Development, UI/UX Design</p>
-
+                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">I wanted to improve my digital drawing skills, and also focus on UI/UX Design. I learned also about JavaScript, CSS, React and D3.js, so I'm really satisfied after this project</p>
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">marinec@kth.se</p>
                                 <a href="https://www.linkedin.com/in/marine-czaplinski/" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
 
