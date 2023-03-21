7a60b177edd654d381bac014601b334ca017169c
Hymalide <marine50799@gmail.com>
2023-03-21 11:09:06 +0100

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,11 +11,11 @@ function About() {
                         <div className="column" style={{ display: 'inline-block', width: '33%', alignItems: 'center' }}>
                             <div className="card">
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
-                                    <h2 className="text-center p-2 text-neutral-1000 py-2 font-bold">Carter Smith</h2>
-                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Collection and Processing, Backend Development, Frontend Development, UI/UX Design</p>
+                                    <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Carter Smith</h2>
+                                <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Collection and Processing, Frontend Development, UI/UX Design</p>
 
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">carters@kth.se</p>
-                                <a href="https://www.linkedin.com/in/carterwsmith" className="text-center p-2 text-neutral-600 py-2 font-normal">Linkedin</a>
+                                <a href="https://www.linkedin.com/in/carterwsmith" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
                             </div>
                         </div>
@@ -27,7 +27,7 @@ function About() {
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design</p>
 
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">umeh@kth.se</p>
-                                <a href="https://www.linkedin.com/in/ernest-umeh" className="text-center p-2 text-neutral-600 py-2 font-normal">Linkedin</a>
+                                <a href="https://www.linkedin.com/in/ernest-umeh" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
                             </div>
                         </div>
@@ -39,7 +39,7 @@ function About() {
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design, User test</p>
 
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">msyqasim@kth.se</p>
-                                <a href="https://www.linkedin.com/in/modi-qasim/" className="text-center p-2 text-neutral-600 py-2 font-normal">Linkedin</a>
+                                <a href="https://www.linkedin.com/in/modi-qasim/" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
                                 </div>
                             </div>
                         </div>
@@ -49,7 +49,7 @@ function About() {
                         <div className="column" style={{ display: 'inline-block', width: '33%', alignItems: 'center' }}>
                             <div className="card">
                                 <div className="container" style={{ marginBottom: '1em', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', height: '100%' }}>
-                                <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">Mans Nyman</h2>
+                                <h2 className="text-center p-2 text-neutral-600 py-2 font-bold">M{"\u00E5"}ns Nyman</h2>
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">Data Processing, Frontend Development, UI/UX Design</p>
 
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">mansnym@kth.se</p>
@@ -66,7 +66,7 @@ function About() {
                                     <p className="text-center p-2 text-neutral-600 py-2 font-normal">Frontend Development, UI/UX Design</p>
 
                                 <p className="text-center p-2 text-neutral-600 py-2 font-normal">marinec@kth.se</p>
-                                <a href="https://www.linkedin.com/in/marine-czaplinski/" className="text-center p-2 text-neutral-600 py-2 font-normal">Linkedin</a>
+                                <a href="https://www.linkedin.com/in/marine-czaplinski/" className="text-center p-2 text-blue-600 py-2 font-normal">Linkedin</a>
 
                                 </div>
                             </div>
